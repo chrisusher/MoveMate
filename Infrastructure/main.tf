c5d1cc4704bed59cc550555cfb3c950e3ba6986a
christopher usher <chris.usher23@gmail.com>
2023-05-21 13:03:27 +0100

    Add Cosmos DB to Terraform

--- Infrastructure/main.tf
+++ Infrastructure/main.tf
@@ -6,10 +6,10 @@ terraform {
     }
   }
   backend "azurerm" {
-      resource_group_name  = "cutestcloud-tstate-rg"
-      storage_account_name = "cutestcloudtfstate20930"
-      container_name       = "movemate-state"
-      key                  = "terraform.tfstate"
+    resource_group_name  = "cutestcloud-tstate-rg"
+    storage_account_name = "cutestcloudtfstate20930"
+    container_name       = "movemate-state"
+    key                  = "terraform.tfstate"
   }
 }
 
@@ -20,4 +20,38 @@ provider "azurerm" {
 resource "azurerm_resource_group" "resource_group" {
   name     = "movemate"
   location = "uksouth"
+}
+
+resource "random_integer" "ri" {
+  min = 10000
+  max = 99999
+}
+
+resource "azurerm_cosmosdb_account" "cosmosdb_account" {
+  name                = "movemate-cosmos-db-${random_integer.ri.result}"
+  location            = azurerm_resource_group.resource_group.location
+  resource_group_name = azurerm_resource_group.resource_group.name
+  offer_type          = "Standard"
+  enable_free_tier    = true
+
+  consistency_policy {
+    consistency_level       = "Eventual"
+    max_interval_in_seconds = 300
+    max_staleness_prefix    = 100000
+  }
+
+  geo_location {
+    location          = azurerm_resource_group.resource_group.location
+    failover_priority = 0
+  }
+
+  capabilities {
+    name = "EnableServerless"
+  }
+}
+
+resource "azurerm_cosmosdb_sql_database" "movemate_database" {
+  name                = "movemate-db"
+  resource_group_name = azurerm_resource_group.resource_group.name
+  account_name        = azurerm_cosmosdb_account.cosmosdb_account.name
 }
