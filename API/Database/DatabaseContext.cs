4e11d8e4da18c8f640ef05f652bee48296a3c83a
christopher usher <chris.usher23@gmail.com>
2023-05-21 13:25:12 +0100

    Add Initial EF Core for Cosmos to API

---
+++ API/Database/DatabaseContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ChrisUsher.MoveMate.API.Database;
+
+public class DatabaseContext : DbContext
+{
+    public DatabaseContext()
+    {
+    }
+
+    public DatabaseContext(DbContextOptions<DatabaseContext> options) : base(options)
+    {
+    }
+}
--- API/Program.cs
+++ API/Program.cs
@@ -1,9 +1,26 @@
+using ChrisUsher.MoveMate.API.Database;
 using Microsoft.Azure.Functions.Worker.Extensions.OpenApi.Extensions;
 using Microsoft.Extensions.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+
+var config = new ConfigurationBuilder()
+#if DEBUG
+    .AddJsonFile("local.settings.json")
+#endif
+    .AddEnvironmentVariables()
+    .Build();
 
 var host = new HostBuilder()
     .ConfigureFunctionsWorkerDefaults()
     .ConfigureOpenApi()
+    .ConfigureServices(services =>
+    {
+        services.AddDbContext<DatabaseContext>(options =>
+            options.UseCosmos(config["Database__AccountName"], config["Database__Key"], config["Database__DatabaseName"]));
+    })
     .Build();
 
 host.Run();
