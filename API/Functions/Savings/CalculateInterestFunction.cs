f2717e9e6b7848940a42a61925fe9cc6cd16472f
Chris Usher <chrisushertester@gmail.com>
2023-06-14 23:26:03 +0100

    Create Function to calculate interest on Monthly Basis

---
+++ API/Functions/Savings/CalculateInterestFunction.cs
@@ -0,0 +1,70 @@
+﻿using System.Net;
+using ChrisUsher.MoveMate.API.Services.Accounts;
+using ChrisUsher.MoveMate.API.Services.Savings;
+using ChrisUsher.MoveMate.Shared.DTOs.Savings;
+using Microsoft.OpenApi.Models;
+
+namespace ChrisUsher.MoveMate.API.Functions.Savings;
+
+public class CalculateInterestFunction : HttpFunction
+{
+    private readonly ILogger _logger;
+    private readonly AccountService _accountService;
+    private readonly SavingsService _savingsService;
+    private readonly InterestService _interestService;
+
+    public CalculateInterestFunction(
+        ILoggerFactory loggerFactory,
+        AccountService accountService,
+        SavingsService savingsService,
+        InterestService interestService)
+    {
+        _logger = loggerFactory.CreateLogger<CalculateInterestFunction>();
+        _accountService = accountService;
+        _savingsService = savingsService;
+        _interestService = interestService;
+    }
+    
+    [OpenApiOperation(operationId: "CalculateInterest", tags: new[] { "Savings" }, Summary = "")]
+    [OpenApiResponseWithBody(HttpStatusCode.OK, "application/json", typeof(SavingsInterestBreakdown))]
+    [OpenApiParameter(name: "accountId", In = ParameterLocation.Path, Required = true, Type = typeof(Guid))]
+    [OpenApiParameter(name: "savingsId", In = ParameterLocation.Path, Required = true, Type = typeof(Guid))]
+    [Function("CalculateInterest")]
+    public async Task<HttpResponseData> CalculateInterest([HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = "Accounts/{accountId}/Savings/{savingsId}/CalculateInterest")] HttpRequestData request,
+        Guid accountId,
+        Guid savingsId)
+    {
+        HttpResponseData response;
+
+        try
+        {
+            response = request.CreateResponse(HttpStatusCode.OK);
+
+            var account = await _accountService.GetAccountAsync(accountId);
+            var savingsAccount = await _savingsService.GetSavingsAccountAsync(accountId, savingsId);
+
+            var interestEndDate = DateTime.UtcNow;
+
+            if(account.EstimatedSaleDate.HasValue)
+            {
+                interestEndDate = account.EstimatedSaleDate.Value;
+            }
+
+            var interestResponse = _interestService.CalculateInterest(savingsAccount, interestEndDate);
+            await response.WriteAsJsonAsync(interestResponse);
+        }
+        catch (DataNotFoundException dataNotFound)
+        {
+            response = request.CreateResponse(HttpStatusCode.NotFound);
+            await response.WriteStringAsync(dataNotFound.Message);
+
+            return response;
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(new EventId(Convert.ToInt32(DateTime.UtcNow.ToString("HHmmss"))), ex, ex.Message);
+            throw;
+        }
+        return response;
+    }
+}
---
+++ Shared/DTOs/Savings/MonthlyInterestBreakdown.cs
@@ -0,0 +1,16 @@
+﻿namespace ChrisUsher.MoveMate.Shared;
+
+public class MonthlyInterestBreakdown
+{
+    [JsonPropertyName("interest")]
+    public double Interest { get; set; }
+
+    [JsonPropertyName("balance")]
+    public double Balance { get; set; }
+
+    [JsonPropertyName("deposits")]
+    public double Deposits { get; set; }
+
+    [JsonPropertyName("date")]
+    public DateTime Date { get; set; }
+}
