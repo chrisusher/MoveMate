be71a92bafadb3a65a01a0c900245eda42f1999c
Chris Usher <chrisushertester@gmail.com>
2023-07-10 22:02:44 +0100

    Add Purchase and Sale Price to Property Viability Report

--- API/Functions/Reports/PropertyViabilityReportFunction.cs
+++ API/Functions/Reports/PropertyViabilityReportFunction.cs
@@ -25,36 +25,27 @@ public class PropertyViabilityReportFunction : HttpFunction
     }
 
     [OpenApiOperation(operationId: "CreatePropertyViabilityReport", tags: new[] { "Reports" }, Summary = "")]
+    [OpenApiRequestBody("application/json", typeof(PropertyViabilityReportRequest))]
     [OpenApiResponseWithBody(HttpStatusCode.OK, "application/json", typeof(PropertyViabilityReport))]
     [OpenApiParameter(name: "accountId", In = ParameterLocation.Path,  Required = true, Type = typeof(Guid))]
     [OpenApiParameter(name: "propertyId", In = ParameterLocation.Path,  Required = true, Type = typeof(Guid))]
-    [OpenApiParameter(name: "interestRate", In = ParameterLocation.Query,  Required = true, Type = typeof(double))]
-    [OpenApiParameter(name: "caseModel", In = ParameterLocation.Query,  Required = true, Type = typeof(CaseType))]
-    [OpenApiParameter(name: "years", In = ParameterLocation.Query,  Required = true, Type = typeof(int))]
     [Function("CreatePropertyViabilityReport")]
-    public async Task<HttpResponseData> CalculateInterest([HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "Reports/PropertyViability/{accountId}/{propertyId}")] HttpRequestData request,
+    public async Task<HttpResponseData> CalculateInterest([HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = "Reports/PropertyViability/{accountId}/{propertyId}")] HttpRequestData request,
         Guid accountId,
-        Guid propertyId,
-        double interestRate,
-        string caseModel,
-        int years)
+        Guid propertyId)
     {
         HttpResponseData response;
 
         try
         {
-            var caseType = CaseType.MiddleCase;
-
-            if(!string.IsNullOrEmpty(caseModel))
-            {
-                caseType = Enum.Parse<CaseType>(caseModel, true);
-            }
+            var requestContent = await request.ReadAsStringAsync();
+            var requestBody = JsonSerializer.Deserialize<PropertyViabilityReportRequest>(requestContent);
 
             response = request.CreateResponse(HttpStatusCode.OK);
 
             var property = await _propertyService.GetPropertyAsync(accountId, propertyId);
 
-            var report = await _reportService.GetPropertyViabilityReport(property, interestRate, caseType, years);
+            var report = await _reportService.GetPropertyViabilityReport(property, requestBody);
             await response.WriteAsJsonAsync(report);
         }
         catch (DataNotFoundException dataNotFound)
--- Shared/DTOs/Properties/Property.cs
+++ Shared/DTOs/Properties/Property.cs
@@ -1,10 +1,13 @@
 ﻿namespace ChrisUsher.MoveMate.Shared.DTOs.Properties;
 
-public class Property : UpdatePropertyRequest 
+public class Property : UpdatePropertyRequest
 {
     [JsonPropertyName("propertyId")]
     public Guid PropertyId { get; set; }
-    
+
     [JsonPropertyName("accountId")]
     public Guid AccountId { get; set; }
+
+    [JsonPropertyName("purchasePrice")]
+    public double? PurchasePrice { get; set; }
 }
