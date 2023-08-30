f6752e1a064f23afc293d19565ae0265126518d4
Chris Usher <chrisushertester@gmail.com>
2023-08-30 22:09:13 +0100

    Ensure JSON Properties are camel-cased (#17)

--- Shared/DTOs/Savings/AccountBalance.cs
+++ Shared/DTOs/Savings/AccountBalance.cs
@@ -2,8 +2,10 @@ namespace ChrisUsher.MoveMate.Shared.DTOs.Savings
 {
     public class AccountBalance
     {
+        [JsonPropertyName("created")]
         public DateTime Created { get; set; }
 
+        [JsonPropertyName("balance")]
         public double Balance { get; set; }
     }
 }
