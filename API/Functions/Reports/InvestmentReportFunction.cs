0a3d7684dc7b52be82c95cc2428a657e68837f21
Chris Usher <chrisushertester@gmail.com>
2024-06-11 16:54:09 +0100

    Add a Stocks and Shares Investment Report to the FunctionApp (#33)
    * Add Function to be able to create a Report on all Investments
    
    * Make sure we don't create Reports if we don't pass in a Stocks and Shares Account
    
    * Refactor common access of Savings Service into a Base class

---
+++ API/Functions/Reports/InvestmentReportFunction.cs
@@ -0,0 +1,63 @@
+using System.Net;
+using ChrisUsher.MoveMate.API.Services.Reports;
+using ChrisUsher.MoveMate.Shared.DTOs.Reports;
+using ChrisUsher.MoveMate.Shared.Enums;
+using Microsoft.OpenApi.Models;
+
+namespace ChrisUsher.MoveMate.API.Functions.Reports;
+
+public class InvestmentReportFunction : HttpFunction
+{
+    private readonly ReportsService _reportsService;
+    private readonly ILogger<InvestmentReportFunction> _logger;
+
+    public InvestmentReportFunction(
+        ILoggerFactory loggerFactory,
+        ReportsService reportsService
+    )
+    {
+        _reportsService = reportsService;
+        _logger = loggerFactory.CreateLogger<InvestmentReportFunction>();
+    }
+
+    [OpenApiOperation(operationId: "InvestmentReportFunction", tags: new[] { "Reports" }, Summary = "")]
+    [OpenApiResponseWithBody(HttpStatusCode.OK, "application/json", typeof(SavingsReport))]
+    [OpenApiParameter(name: "accountId", In = ParameterLocation.Path, Required = true, Type = typeof(Guid))]
+    [OpenApiParameter(name: "savingsId", In = ParameterLocation.Path, Required = true, Type = typeof(Guid))]
+    [Function("InvestmentReportFunction")]
+    public async Task<HttpResponseData> CreateReportAsync([HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "Reports/InvestmentReport/{accountId}/{savingsId}")] HttpRequestData request,
+        Guid accountId,
+        Guid savingsId)
+    {
+        HttpResponseData response;
+
+        try
+        {
+            response = request.CreateResponse(HttpStatusCode.OK);
+
+            var report = await _reportsService.GetStockInvestmentReportAsync(accountId, savingsId);
+            
+            await response.WriteAsJsonAsync(report);
+        }
+        catch (DataNotFoundException dataNotFound)
+        {
+            response = request.CreateResponse(HttpStatusCode.NotFound);
+            await response.WriteStringAsync(dataNotFound.Message);
+
+            return response;
+        }
+        catch (InvalidRequestException invalidRequest)
+        {
+            response = request.CreateResponse(HttpStatusCode.BadRequest);
+            await response.WriteStringAsync(invalidRequest.Message);
+
+            return response;
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(new EventId(Convert.ToInt32(DateTime.UtcNow.ToString("HHmmss"))), ex, ex.Message);
+            throw;
+        }
+        return response;
+    }
+}
